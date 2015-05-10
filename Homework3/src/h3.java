f6066a3bf8c4ac2bbe4ab2c86a58a4d7a3810dbb
nikolaydobrev <ndobrevgw@gmail.com>
2015-05-10 19:00:51 +0300

    h

--- Homework3/src/h3.java
+++ Homework3/src/h3.java
@@ -6,22 +6,26 @@ public class h3 {
 		Scanner input=new Scanner(System.in, "UTF-8");
 		
 		System.out.println("Enter 5 numbers");
-		int num;	
-		int sum=0;
-		int count=0;
-		while(true){
-			num=input.nextInt();
-			count++;
-			sum+=num;
-			if(count==5){
-			
-				System.out.println(sum);
-				break;
-			
+		int num=5;
+		int[] arr=new int[num];
+		
+		int i=0, swap=0;
+		int k=0; 
+		for(i=0;i<num;i++)
+		arr[i]=input.nextInt();
+		for(i=0;i<num-1;i++){
+			for(k=0;k<(num-1)-i;k++){
+				if(arr[k]>arr[k+1]){
+					swap=arr[k];
+					arr[k]=arr[k+1];
+					arr[k+1]=swap;
+				}
 			}
-	
+		}
+		System.out.println(arr[0]);
+		System.out.println(arr[num-1]);
+		
 	}
-}
 }
 
 
